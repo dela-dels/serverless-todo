ad8300755122d58cf9d6ab3e737a98f8f9744cd7
Dela Akakpo <delprane14@gmail.com>
2023-03-14 15:53:40 +0000

    updates client side with authO urls

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,11 +1,8 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = 'pb8lb1wvc1'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map. For example:
-  // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-vgfer0miwt7v0zxv.eu.auth0.com', // Auth0 domain
+  clientId: '38IpIKShSGfu6ENGp5kOFXXRBG9cIe2h', // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
